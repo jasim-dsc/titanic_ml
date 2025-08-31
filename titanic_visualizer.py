d52457c8a5a78484a8ab4574d5e74ac797b7d874
Jasim Sanni <sannijasim23@gmail.com>
2025-08-31 19:05:14 +0100

    Visualization Script

--- titanic_visualizer.py
+++ titanic_visualizer.py
@@ -3,34 +3,34 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Load cleaned dataset
+#Load cleaned dataset
 data = pd.read_csv('titanic_cleaned.csv')
 
-# Set style
+#Set style
 sns.set_style('whitegrid')
 
-# Survival Count Plot
+#Survival Count Plot
 plt.figure(figsize=(6,4))
 sns.countplot(x='Survived', data=data, palette='Blues')
 plt.title('Survival Count (0 = No, 1 = Yes)')
 plt.savefig('survival_count.png')
 plt.close()
 
-# Survival by Gender
+#Survival by Gender
 plt.figure(figsize=(6,4))
 sns.countplot(x='Sex', hue='Survived', data=data, palette='Set2')
 plt.title('Survival by Gender')
 plt.savefig('survival_by_gender.png')
 plt.close()
 
-# Survival by Class
+#Survival by Class
 plt.figure(figsize=(6,4))
 sns.countplot(x='Pclass', hue='Survived', data=data, palette='Set1')
 plt.title('Survival by Passenger Class')
 plt.savefig('survival_by_class.png')
 plt.close()
 
-# Age Distribution
+#Age Distribution
 plt.figure(figsize=(6,4))
 sns.histplot(data['Age'], bins=30, kde=True, color='purple')
 plt.title('Age Distribution of Passengers')
